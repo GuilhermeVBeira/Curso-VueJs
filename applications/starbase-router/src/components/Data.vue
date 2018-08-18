6b92d35d0626c663db91c5c87a8ced5a29685171
GuilhermeVBeira <gui_doggao@hotmail.com>
2018-08-18 20:44:45 -0300

    Introducing VueRouter

---
+++ applications/starbase-router/src/components/Data.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="">
+    Type: {{type}}
+</div>
+</template>
+
+<scritp>
+export default {
+    data() {
+        return {
+            type: this.$route.params.type
+        }
+    }
+}
+</scritp>
